bc183f69bd8d62264f5df7dad4d0a5e3ecd3c78c
utcreator <utcreator@gmail.com>
2025-10-25 14:01:05 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const cacheName = 'radhe-krishna-counter-v1';
+const filesToCache = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request))); });
