29f5d3426d29f8bf752b8f30e8b9a3978afad680
utcreator <utcreator@gmail.com>
2025-10-25 11:20:21 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "radhe-counter-v1";
+const FILES_TO_CACHE = [
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
