84c64ceefb98789441071fc9e41ce137df7a0a6b
utcreator <utcreator@gmail.com>
2025-10-25 13:14:33 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,4 @@
-const CACHE_NAME = "radhe-counter-v1";
-const FILES_TO_CACHE = [
-  "index.html",
-  "manifest.json",
-  "icon-192.png",
-  "icon-512.png"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+const cacheName = 'radhe-krishna-counter-v1';
+const filesToCache = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request))); });
