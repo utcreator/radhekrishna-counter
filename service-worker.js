ad6bbca80eea3744a0a2c69b738307d0d0ffbcd1
utcreator <utcreator@gmail.com>
2025-10-28 12:35:29 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,45 @@
-const cacheName = 'radhe-krishna-counter-v1';
-const filesToCache = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE_NAME = 'krishna-counter-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
+// Install SW and cache core files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
+// Activate and remove old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(cacheNames.map((name) => {
+        if (name !== CACHE_NAME) return caches.delete(name);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch and serve from cache first
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).then((resp) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, resp.clone());
+            return resp;
+          });
+        })
+      );
+    })
+  );
 });
